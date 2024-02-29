df91bdedac7fe751a1c6ab0a84d0428973b5e8fd
Amr Alloush <amr.alloush@riskident.com>
2024-02-29 14:22:52 +0100

    upgrade dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -7,15 +7,15 @@ object Build {
 
   lazy val Version = new {
     val h2             = "2.2.224"
-    val slf4j          = "2.0.9"
-    val zio            = "2.0.18"
+    val slf4j          = "2.0.12"
+    val zio            = "2.0.21"
     val cats           = "2.10.0"
     val scalaCheck     = "1.17.0"
     val slick          = "3.4.1"
     val shapeless      = "2.3.10"
     val shapelessCheck = "1.3.0"
 
-    lazy val scala213 = List("2.13.10", "2.13.9", "2.13.12")
+    lazy val scala213 = List("2.13.10", "2.13.9", "2.13.13")
     lazy val scala212 = List("2.12.15", "2.12.16", "2.12.17", "2.12.18")
   }
 
